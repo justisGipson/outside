3871a9360b67fe9073652617090783609a1ee6c8
Justis Gipson <justis710@gmail.com>
2019-08-12 13:34:18 -0400

    updated package.json

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
+const minimist = require('minimist');
 
 module.exports = () => {
+    const args = minimist(process.argv.slice(2))
+    const cmd = args._[0]
+    
     console.log("Welcome Outside")
 }
